87ff5ca4f0c1d7d7ae64d71a6ece140a637632a5
Vitaliy Antipov <demjke64@gmail.com>
2024-12-12 11:46:36 +0400

    вывод товаров с помощью json файла

--- js/script.js
+++ js/script.js
@@ -15,12 +15,47 @@ document.addEventListener("DOMContentLoaded", function () {
             e.target.closest(".products-charact__item").classList.toggle("active");
         }
     });
-});
-const aboutSlider = new Swiper(".main-slider", {
-    loop: true,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: ".main-slider .swiper-button-next",
-        prevEl: ".main-slider .swiper-button-prev",
-    },
+
+    const productsHome = document.querySelector(".products-wrapper");
+    if (productsHome) {
+        fetch("js/data.json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to load JSON: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                renderProducts(data);
+            })
+            .catch(error => {
+                console.error("Ошибка загрузки JSON:", error);
+            });
+    }
+
+    function renderProducts(data) {
+        data.forEach(item => {
+            const product = document.createElement("div");
+            product.className = "products-item";
+            product.dataset.productId = item.id;
+            product.dataset.category = item.category;
+            product.innerHTML = `
+            <div class="products-item__img">
+                <img src="${item.image}" alt="" />
+                <div class="products-item__btn-wrapper">
+                    <div class="products-item__btn">
+                        <img src="img/home/cart.svg" alt="" />
+                        <span>Add to Cart</span>
+                    </div>
+                </div>
+            </div>
+            <div class="products-item__content">
+                <a href="products.html" class="products-item__title">${item.title}</a>
+                <div class="products-item__decs">${item.description}</div>
+                <div class="products-item__price">$${item.price}</div>
+            </div>
+            `;
+            productsHome.appendChild(product);
+        });
+    }
 });
